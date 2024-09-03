846e73b26e294f8435025bb87db258ba46917f6a
yahir <yahir@LAPTOP-H2INCEOF>
2024-09-02 20:17:20 -0600

    Agregar archivos de proyecto.

---
+++ Vectores/Vectores.py
@@ -0,0 +1,21 @@
+# Declara una lista de cadenas
+aves = ["loro gris", "Paloma de diamante", "Coctel"]
+print("Los valores de la lista antes de insertar:")
+
+# Itera sobre la lista antes de imprimir los valores
+for ave in aves:
+    print(ave)
+
+
+
+# Agrega tres valores al final de la lista utilizando el método append()
+aves.append("Mayna")
+aves.append("Periquillos")
+aves.append("Cacatua")
+
+print("Los valores de la lista después de insertar:")
+
+for ave in aves:
+    print(ave)
+
+    print("\n")
